98810ccf95f1ea15f3a6c942ce8ca767c14dade0
kkgosha <kinnisgosha@gmail.com>
2021-07-04 03:19:11 -0400

    Create inputProgram.cpp
    here is my dummy i/o file...

---
+++ inputProgram.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    string first_name, last_name;   
+
+    cout<<"enter your first name";
+    cin>>first_name;
+
+    cout<<"enter your last_name name";
+    cin>>last_name;  
+
+    cout<<"Your full name is " << first_name << " " << last_name << endl;
+
+    return 0;
+}
